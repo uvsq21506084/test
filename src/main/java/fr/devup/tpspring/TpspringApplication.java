6bfff8b930ff9e72af55fd00c422c0f7441be2c9
louloubas <loubna.basskar@gmail.com>
2019-07-04 09:34:50 +0200

    premier commit

---
+++ src/main/java/fr/devup/tpspring/TpspringApplication.java
@@ -0,0 +1,13 @@
+package fr.devup.tpspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/devup/tpspring/TpspringApplicationTests.java
@@ -0,0 +1,16 @@
+package org.devup.tpspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TpspringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
